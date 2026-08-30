41db579bac0cdf28d367fe277268de9d89d76d31
Th-M-Salah <thbaiee@gmail.com>
2026-08-30 09:56:03 +0300

    Update Android app name icons and MainActivity

--- android/app/src/main/java/com/mawaqit/almowaleen/MainActivity.java
+++ android/app/src/main/java/com/mawaqit/almowaleen/MainActivity.java
@@ -1,5 +1,16 @@
 package com.mawaqit.almowaleen;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebSettings settings = bridge.getWebView().getSettings();
+        settings.setMediaPlaybackRequiresUserGesture(false);
+    }
+}
